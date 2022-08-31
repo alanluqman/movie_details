900767aff58756e2ea0d92414873491138a80fbe
alanluqman <35001742+alanluqman@users.noreply.github.com>
2022-08-31 18:58:23 +0300

    Implement search box

--- src/Pages/Movies.js
+++ src/Pages/Movies.js
@@ -1,12 +1,14 @@
+/* eslint-disable array-callback-return */
 /* eslint-disable react/jsx-one-expression-per-line */
 import axios from 'axios';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
 import { fetchMovies } from '../Redux/movies';
 import '../style/Movies.css';
 
 function Movies() {
+  const [search, setSearch] = useState('');
   const movies = useSelector((state) => state.movies);
   const dispatch = useDispatch();
   const api = 'https://api.tvmaze.com/shows';
@@ -21,11 +23,18 @@ function Movies() {
     <div className="movieListHolder">
       <div className="header">
         <h1>Movie List</h1>
-        <input className="search-box" placeholder="Search..." />
+        <input className="search-box" placeholder="Search..." onChange={(event) => { setSearch(event.target.value); }} />
       </div>
       <ul className="moviesDisplay">
         {
-        movies.map((movie) => (
+        // eslint-disable-next-line consistent-return
+        movies.filter((element) => {
+          if (search === '') {
+            return element;
+          } if (element.name.toLowerCase().includes(search.toLowerCase())) {
+            return element;
+          }
+        }).map((movie) => (
           <li key={movie.id}>
             <Link to={`/${movie.name}`}>
               <img className="poster" src={movie.image.medium} alt="poster" />
